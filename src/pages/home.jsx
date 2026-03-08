be91f123ba2b6d3b33b395d20f897f894d018f1c
Andressa Martins <drekamartins@hotmail.com>
2026-03-08 20:46:18 -0300

    fixed minor things

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -11,16 +11,18 @@ export function Home() {
   }, []);
 
   return (
-    <div>
-      <h1>Animais para Adoção</h1>
-      {animals.map(animal => (
-        <div key={animal.id}>
-          <h3>{animal.name}</h3>
-          <p>Espécie: {animal.species}</p>
-          <p>{animal.description}</p>
-          <hr />
-        </div>
-      ))}
+    <div className="container">
+      <h1 className="title">Animais para Adoção</h1>
+
+      <div className="animals-grid">
+        {animals.map(animal => (
+          <div className="animal-card" key={animal.id}>
+            <h3>{animal.name}</h3>
+            <p><strong>Espécie:</strong> {animal.species}</p>
+            <p>{animal.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
